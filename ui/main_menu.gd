a3b4742dfa475d23ca73ef70ce4ab624654d4e3c
contentseeker10 <appelsene.design@gmail.com>
2026-05-22 10:27:50 +0300

    add Register panel

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -1,6 +1,8 @@
 extends Control
 
-@onready var settings_panel = $SettingsPanel
+@onready var settings_panel: Control = $SettingsPanel
+@onready var login_panel: Control = $LoginPanel
+@onready var register_panel: Control = $RegisterPanel
 
 func _ready() -> void:
 	pass
@@ -14,3 +16,11 @@ func _on_settings_button_toggled(toggled_on: bool) -> void:
 		settings_panel.show()
 	else:
 		settings_panel.hide()
+
+func _on_register_button_pressed() -> void:
+	login_panel.hide()
+	register_panel.show()
+
+func _on_back_button_pressed() -> void:
+	register_panel.hide()
+	login_panel.show()
