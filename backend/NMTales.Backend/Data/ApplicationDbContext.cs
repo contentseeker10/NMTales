ff6c0e73fd2baa559a7f29d4eb08b76521a7b3c7
Artem Rozborskyi <artemrozbor@gmail.com>
2026-06-12 19:06:14 +0300

    refactor (AuthController), add dependencies to Program, refactor ApplicationDbContext, fix NotebookController

--- backend/NMTales.Backend/Data/ApplicationDbContext.cs
+++ backend/NMTales.Backend/Data/ApplicationDbContext.cs
@@ -16,18 +16,35 @@ public class ApplicationDbContext : DbContext
     public DbSet<UserQuest> UserQuests { get; set; }
     public DbSet<UserTestSession> UserTestSessions { get; set; }
     public DbSet<NotebookPage> NotebookPages { get; set; }
+    public DbSet<Location> Locations { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // Enforce unique usernames
         modelBuilder.Entity<User>()
             .HasIndex(user => user.Username)
             .IsUnique();
         
+        // Cascade delete: If User dies, their NotebookPages die
         modelBuilder.Entity<NotebookPage>()
             .HasOne(p => p.User)
             .WithMany()
             .HasForeignKey(p => p.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Cascade delete: If User dies, their TestSessions die
+        modelBuilder.Entity<UserTestSession>()
+            .HasOne(s => s.User)
+            .WithMany()
+            .HasForeignKey(s => s.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Cascade delete: If Question dies, its Answers die
+        modelBuilder.Entity<Answer>()
+            .HasOne(a => a.Question)
+            .WithMany(q => q.Answers)
+            .HasForeignKey(a => a.QuestionId)
             .OnDelete(DeleteBehavior.Cascade);
     }
 }
